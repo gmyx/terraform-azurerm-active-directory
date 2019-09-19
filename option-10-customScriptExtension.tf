bb43625d51055e27f66631def8e3e6f5ec075660
Guy Moreau <guy.moreau2@tpsgc-pwgsc.gc.ca>
2019-09-19 08:09:17 -0400

    Change in var names

--- option-10-customScriptExtension.tf
+++ option-10-customScriptExtension.tf
@@ -15,14 +15,14 @@ resource "azurerm_virtual_machine_extension" "CustomScriptExtension" {
   count                = "${var.custom_data == null ? 0 : 1}"
   name                 = "CustomScriptExtension"
   location             = "${var.location}"
-  resource_group_name  = "${var.resource_group_name}"
-  virtual_machine_name = "${azurerm_virtual_machine.VM.name}"
+  resource_group_name  = "${var.resourceGroupName}"
+  virtual_machine_name = "${azurerm_virtual_machine.dc1.name}"
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
 
   settings = <<SETTINGS
-        {   
+        {
         "commandToExecute": "powershell -command copy-item \"c:\\AzureData\\CustomData.bin\" \"c:\\AzureData\\CustomData.ps1\";\"c:\\AzureData\\CustomData.ps1\""
         }
 SETTINGS
